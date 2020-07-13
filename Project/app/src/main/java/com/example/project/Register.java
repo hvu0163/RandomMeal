de39d316a4b2838f84e5870fa99d0b58e70073d4
sonnh <yggdrasil2769@gmail.com>
2020-07-13 20:05:02 +0700

    co ham login va register roi, code vao trong day thoi

--- Project/app/src/main/java/com/example/project/Register.java
+++ Project/app/src/main/java/com/example/project/Register.java
@@ -2,13 +2,26 @@ package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Register extends AppCompatActivity {
-
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+    }
+
+    public void cancel_on_click(View view) {
+        button = findViewById(R.id.cancel_button);
+        Intent intent = new Intent(this, Login.class);
+        this.startActivity(intent);
+    }
+
+    public void register_on_click(View view) {
+
     }
 }
