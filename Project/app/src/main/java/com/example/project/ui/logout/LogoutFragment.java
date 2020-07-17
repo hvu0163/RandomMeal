26ea83274d896d958ea497c0c20dd29e0cc72703
sonnh <yggdrasil2769@gmail.com>
2020-07-17 10:52:09 +0700

    update

--- Project/app/src/main/java/com/example/project/ui/logout/LogoutFragment.java
+++ Project/app/src/main/java/com/example/project/ui/logout/LogoutFragment.java
@@ -1,4 +1,4 @@
-package com.example.project.ui.home;
+package com.example.project.ui.logout;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -14,22 +14,22 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.example.project.R;
 
-public class HomeFragment extends Fragment {
+public class LogoutFragment extends Fragment {
 
-    private HomeViewModel homeViewModel;
+    private LogoutViewModel logoutViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
+        logoutViewModel =
+                ViewModelProviders.of(this).get(LogoutViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
 //        final TextView textView = root.findViewById(R.id.text_home);
-//        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-//            @Override
-//            public void onChanged(@Nullable String s) {
-//                textView.setText(s);
-//            }
-//        });
+////        logoutViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+//////            @Override
+//////            public void onChanged(@Nullable String s) {
+//////                textView.setText(s);
+//////            }
+//////        });
         return root;
     }
 }
--- Project/app/src/main/java/com/example/project/ui/logout/LogoutViewModel.java
+++ Project/app/src/main/java/com/example/project/ui/logout/LogoutViewModel.java
@@ -1,16 +1,16 @@
-package com.example.project.ui.slideshow;
+package com.example.project.ui.logout;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class SlideshowViewModel extends ViewModel {
+public class LogoutViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public SlideshowViewModel() {
+    public LogoutViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is slideshow fragment");
+        mText.setValue("This is home fragment");
     }
 
     public LiveData<String> getText() {
