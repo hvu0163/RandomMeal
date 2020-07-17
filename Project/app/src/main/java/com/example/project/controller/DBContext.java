c2a87713fd3c3a83d19ffb79229096227f929443
sonnh <yggdrasil2769@gmail.com>
2020-07-17 10:09:27 +0700

    update

--- Project/app/src/main/java/com/example/project/controller/DBContext.java
+++ Project/app/src/main/java/com/example/project/controller/DBContext.java
@@ -1,4 +1,4 @@
-package com.example.project;
+package com.example.project.controller;
 
 import android.annotation.SuppressLint;
 import android.content.ContentValues;
--- Project/app/src/main/java/com/example/project/controller/Register.java
+++ Project/app/src/main/java/com/example/project/controller/Register.java
@@ -1,14 +1,14 @@
-package com.example.project;
+package com.example.project.controller;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.project.R;
 import com.example.project.model.Account;
 import com.example.project.model.UserInformation;
 
