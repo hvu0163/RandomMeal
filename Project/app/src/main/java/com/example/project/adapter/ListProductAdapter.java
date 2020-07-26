030c89a2ee338c5a0daff962729418c8a3c29fb7
sonnh <yggdrasil2769@gmail.com>
2020-07-26 20:56:35 +0700

    update

---
+++ Project/app/src/main/java/com/example/project/adapter/ListProductAdapter.java
@@ -0,0 +1,75 @@
+package com.example.project.adapter;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.example.project.R;
+import com.example.project.model.Dishes;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListProductAdapter extends BaseAdapter {
+
+
+    private Context context;
+    private int layout;
+    private List<Dishes> dishList;
+
+    public ListProductAdapter(Context context, int layout, List<Dishes> dishList) {
+        this.context = context;
+        this.layout = layout;
+        this.dishList = dishList;
+    }
+
+    @Override
+    public int getCount() {
+        return dishList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return dishList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageProduct;
+        TextView txtProductName;
+        TextView txtDetail;
+
+        if (convertView == null) {
+            convertView = LayoutInflater.from(context).inflate(R.layout.item_display, parent, false);
+
+        }
+        imageProduct = convertView.findViewById(R.id.image_display);
+        txtProductName = convertView.findViewById(R.id.title1);
+        txtDetail = convertView.findViewById(R.id.title2);
+        Bitmap bm = null;
+        try {
+            URL url = new URL(dishList.get(position).getUrl());
+            bm = BitmapFactory.decodeStream(url.openConnection().getInputStream());
+        }catch(Exception e) {
+
+        }
+        Glide.with(convertView).load(dishList.get(position).getUrl()).into(imageProduct);
+        txtProductName.setText(dishList.get(position).getName());
+        txtDetail.setText(dishList.get(position).getUrl());
+        return convertView;
+    }
+}
--- Project/app/src/main/java/com/example/project/ui/home/HomeFragment.java
+++ Project/app/src/main/java/com/example/project/ui/home/HomeFragment.java
@@ -4,7 +4,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.GridView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,10 +16,16 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.project.R;
+import com.example.project.adapter.ListProductAdapter;
+import com.example.project.model.Dishes;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
+    private GridView gridView = null;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -30,6 +39,28 @@ public class HomeFragment extends Fragment {
 //                textView.setText(s);
 //            }
 //        });
+        List<Dishes> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            Dishes d = new Dishes();
+            d.setName("abc");
+            d.setUrl("https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Korean_acorn_jelly-Dotorimuk-03.jpg/1200px-Korean_acorn_jelly-Dotorimuk-03.jpg");
+            list.add(d);
+        }
+        gridView = root.findViewById(R.id.gridview);
+        ListProductAdapter abc = new ListProductAdapter(root.getContext(), R.layout.fragment_home, list);
+        gridView.setAdapter(abc);
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(view.getContext(), position, Toast.LENGTH_SHORT).show();
+
+            }
+        });
         return root;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+
     }
 }
